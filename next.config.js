1d93c68db4f3976e4d90d0768de58e28633b8df7
Marty <martyumlas@gmail.com>
2024-01-10 14:04:37 +0800

    enable docker and hot reload

--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+module.exports = {
+    webpackDevMiddleware: (config) => {
+      config.watchOptions = {
+        poll: 1000, // Check for changes every second
+        aggregateTimeout: 300, // Delay before rebuilding
+      };
+      return config;
+    },
+  };
